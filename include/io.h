2a6183e01fed43c2f3c8c4389e5bb4dc5aa4fafa
Farhan Naqib <fn72@cornell.edu>
2025-07-20 00:36:00 -0400

    add small changes

--- include/io.h
+++ include/io.h
@@ -1,10 +1,13 @@
 #define PERIPHERAL_BASE 0x3F000000
 
+/** writes 4-byte val to register reg */
 void mmio_write(long reg, unsigned int val);
+/** reads 4-byte val from register reg */
 unsigned int mmio_read(long reg);
 
 void delay(unsigned int count);
 
+/** must be ran before using uart funcs */
 void uart_init();
 void uart_write_char(unsigned char ch);
 void uart_write_text(char *buffer);
--- src/kernel.c
+++ src/kernel.c
@@ -8,22 +8,20 @@ void main(int core)
     if (core == 0) {
         uart_init();
         fb_init();
-
-        // *(volatile unsigned long *)0xE0 = (unsigned long)&main;
+        
         mmio_write(0xE0, (unsigned long)_start);
+        mmio_write(0xE8, (unsigned long)_start);
+        mmio_write(0xF0, (unsigned long)_start);
         asm volatile ("sev");
     }
-
-    delay(core * 100000);
+    
+    draw_char('0' + core, 50, 50 + 20 * core, 0x00FFFFFF, 2);
+    
+    delay(core * 100000); // bc of writing text
     
     uart_write_text("Hello from processor ");
     uart_write_char(core + '0');
     uart_write_char('\n');
     
-
-    if (core == 0) {
-        draw_pixel(1, 1, 0x00FFFFFF);
-        draw_string(5, 5, "fortnite", 0x00FFFFFF, 5);
-    }
     while (1);
 }
